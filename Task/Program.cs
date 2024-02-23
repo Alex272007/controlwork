0ed0f623d32176b150e1f6119d929a5ace0864ea
Aleksey Kvashnin <aleks272007@gmail.com>
2024-02-23 20:42:25 +0400

    Добавили код для решеня задачи

---
+++ Task/Program.cs
@@ -0,0 +1,32 @@
+﻿    Console.WriteLine("Введите элементы массива через запятую:"); 
+        string[] originalArray = Console.ReadLine().Split(',');
+ 
+        int count = 0;
+        for (int i = 0; i < originalArray.Length; i++) // Определяем размер нового массива
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+ 
+        string[] newArray = new string[count]; // Создаем новый массив
+
+        
+        int index = 0; // Заполняем новый массив
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                newArray[index] = originalArray[i];
+                index++;
+            }
+        }
+
+        Console.WriteLine("Новый массив:");
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            Console.Write(newArray[i] + "; ");
+        }
+
+
